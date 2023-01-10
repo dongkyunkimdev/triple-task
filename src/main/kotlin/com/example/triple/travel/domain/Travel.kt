ef14a5daeb32108de10668f8193cbf76c3ccbf4a
dongkyunkimdev <ddkds66@gmail.com>
2023-01-10 18:34:33 +0900

    여행 jpa entity 작성

---
+++ src/main/kotlin/com/example/triple/travel/domain/Travel.kt
@@ -0,0 +1,26 @@
+package com.example.triple.travel.domain
+
+import com.example.triple.city.domain.City
+import com.example.triple.common.entity.JpaAuditEntity
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "travels")
+class Travel(
+    city: City,
+    startedAt: LocalDateTime,
+    endedAt: LocalDateTime
+) : JpaAuditEntity() {
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    var city: City = city
+        protected set
+
+    @Column(name = "started_at", nullable = false)
+    var startedAt: LocalDateTime = startedAt
+        protected set
+
+    @Column(name = "ended_at", nullable = false)
+    var endedAt: LocalDateTime = endedAt
+        protected set
+}
