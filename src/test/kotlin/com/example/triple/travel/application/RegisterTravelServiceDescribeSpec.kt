93a04eab791b15d6d4e91fd7546bad5f580d66ee
dongkyunkimdev <ddkds66@gmail.com>
2023-01-13 23:26:17 +0900

    여행 등록 application service의 출발일시가 종료일시 이후인 경우의 실패 단위 테스트 케이스 작성

--- src/test/kotlin/com/example/triple/travel/application/RegisterTravelServiceDescribeSpec.kt
+++ src/test/kotlin/com/example/triple/travel/application/RegisterTravelServiceDescribeSpec.kt
@@ -1,6 +1,7 @@
 package com.example.triple.travel.application
 
 import com.example.triple.city.application.CityPersistencePort
+import com.example.triple.city.domain.City
 import com.example.triple.travel.application.exception.CityNotFoundException
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
@@ -22,19 +23,37 @@ class RegisterTravelServiceDescribeSpec : DescribeSpec({
                 }
             }
         }
+
+        context("존재하는 cityId를 가지고") {
+            every { mockCityPersistencePort.findCityById(existsCityIdCommand.cityId) } answers { City("cityName") }
+            context("여행 출발일시가 종료일시 이후인 command가 주어지면") {
+                it("IllegalArgumentException 발생") {
+                    shouldThrow<IllegalArgumentException> {
+                        registerTravelService.command(startedIsAfterEndedCommand)
+                    }
+                }
+            }
+
+        }
     }
 
 }) {
     companion object {
         private val notExistsCityIdCommand = RegisterTravelService.RegisterTravelCommand(
             cityId = "notExistsId",
-            startedAt = LocalDateTime.now(),
+            startedAt = LocalDateTime.now().minusDays(1),
             endedAt = LocalDateTime.now(),
         )
 
         private val existsCityIdCommand = RegisterTravelService.RegisterTravelCommand(
             cityId = "existsId",
-            startedAt = LocalDateTime.now(),
+            startedAt = LocalDateTime.now().minusDays(1),
+            endedAt = LocalDateTime.now(),
+        )
+
+        private val startedIsAfterEndedCommand = RegisterTravelService.RegisterTravelCommand(
+            cityId = "existsId",
+            startedAt = LocalDateTime.now().plusDays(1),
             endedAt = LocalDateTime.now(),
         )
     }
