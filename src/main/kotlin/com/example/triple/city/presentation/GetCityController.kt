3810a454bfca1b41c0ee2b3d96b89cd092967ba2
dongkyunkimdev <ddkds66@gmail.com>
2023-01-14 17:25:18 +0900

    단일 도시 정보 조회 controller 작성

---
+++ src/main/kotlin/com/example/triple/city/presentation/GetCityController.kt
@@ -0,0 +1,30 @@
+package com.example.triple.city.presentation
+
+import com.example.triple.city.application.GetCityService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GetCityController(
+    private val getCityService: GetCityService
+) {
+    @GetMapping("/city/{id}")
+    fun getCity(@PathVariable id: String): ResponseEntity<GetCityResponseDto> {
+        val info = getCityService.command(GetCityService.GetCityCommand(id))
+        return ResponseEntity.ok(GetCityResponseDto.from(info))
+    }
+
+    data class GetCityResponseDto(
+        val id: String,
+        val name: String
+    ) {
+        companion object {
+            fun from(info: GetCityService.GetCityInfo): GetCityResponseDto = GetCityResponseDto(
+                id = info.id,
+                name = info.name
+            )
+        }
+    }
+}
