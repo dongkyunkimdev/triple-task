5312c35a5197a41904f0becb4aae8397412fdca1
dongkyunkimdev <ddkds66@gmail.com>
2023-01-15 01:05:15 +0900

    사용자별 도시 목록 조회 조건을 위한 사용자별 도시 조회 통계 엔티티 작성

---
+++ src/main/kotlin/com/example/triple/city/domain/CityStatistic.kt
@@ -0,0 +1,26 @@
+package com.example.triple.city.domain
+
+import com.example.triple.common.entity.JpaAuditEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name = "travel_statistics")
+class CityStatistic(
+    cityId: String,
+    cityName: String,
+    userId: String
+) : JpaAuditEntity() {
+    @Column(name = "city_id", nullable = false)
+    var cityId: String = cityId
+        protected set
+
+    @Column(name = "city_name", nullable = false)
+    var cityName: String = cityName
+        protected set
+
+    @Column(name = "user_id", nullable = false)
+    var userId: String = userId
+        protected set
+}
