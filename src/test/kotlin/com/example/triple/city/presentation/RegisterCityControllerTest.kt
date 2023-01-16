988d9c6bb7354e58802d62444a60c21c3a178574
dongkyunkimdev <ddkds66@gmail.com>
2023-01-16 17:09:23 +0900

    도시 등록 API의 이름이 중복되어 실패하는 통합 테스트 케이스 작성

--- src/test/kotlin/com/example/triple/city/presentation/RegisterCityControllerTest.kt
+++ src/test/kotlin/com/example/triple/city/presentation/RegisterCityControllerTest.kt
@@ -1,6 +1,8 @@
 package com.example.triple.city.presentation
 
+import com.example.triple.city.application.exception.DuplicatedCityNameException
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -38,6 +40,20 @@ class RegisterCityControllerTest {
                 status().isCreated,
                 jsonPath("name").value(notDuplicatedDto["name"])
             )
+    }
+
+    @Test()
+    @DisplayName("도시 등록에 실패, 이름이 중복됨")
+    fun register_city_fail_duplicated_name() {
+        // given
+        val duplicatedDto = hashMapOf("name" to "duplicatedName")
+        postRequest("/city", duplicatedDto)
+
+        // when
+        val expectedException = assertThatThrownBy { postRequest("/city", duplicatedDto) }
+
+        // then
+        expectedException.cause.isInstanceOf(DuplicatedCityNameException::class.java)
     }
 
     private fun postRequest(requestUrl: String, content: Map<String, String>): ResultActions =
