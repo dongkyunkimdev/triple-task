043408705e0a91ff69ec40253994d526899bf772
dongkyunkimdev <ddkds66@gmail.com>
2023-01-16 16:50:15 +0900

    도시 등록 API의 이름이 중복되어 실패하는 통합 테스트 케이스 작성

---
+++ src/test/kotlin/com/example/triple/city/presentation/RegisterCityControllerTest.kt
@@ -0,0 +1,51 @@
+package com.example.triple.city.presentation
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RegisterCityControllerTest {
+
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Test
+    @DisplayName("도시 등록에 실패, 이름이 중복됨")
+    fun register_city_fail_duplicated_name() {
+        // given
+        val notDuplicatedDto = hashMapOf("name" to "notDuplicatedName")
+
+        // when
+        val actions = postRequest("/city", notDuplicatedDto)
+
+        // then
+        actions
+            .andExpectAll(
+                status().isCreated,
+                jsonPath("name").value(notDuplicatedDto["name"])
+            )
+    }
+
+    private fun postRequest(requestUrl: String, content: Map<String, String>): ResultActions =
+        mvc.perform(
+            post(requestUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(objectMapper.writeValueAsString(content))
+        )
+}
