4cfd6bf46dffe18e36cd09649f0278ad722f9e30
dongkyunkimdev <ddkds66@gmail.com>
2023-01-16 19:05:37 +0900

    도시 단일 정보 삭제 API의 성공하는 통합 테스트 케이스 작성

---
+++ src/test/kotlin/com/example/triple/city/presentation/DeleteCityControllerTest.kt
@@ -0,0 +1,63 @@
+package com.example.triple.city.presentation
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.jsonpath.JsonPath
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DeleteCityControllerTest {
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Test
+    @DisplayName("도시 정보 삭제 API 성공")
+    @Transactional
+    fun delete_city_success() {
+        // given
+        val result = postRequest("/city", hashMapOf("name" to "notDuplicatedName")).andReturn()
+        val savedId = JsonPath.read<String>(result.response.contentAsString, "$.id")
+        val existsIdDto = hashMapOf("id" to savedId)
+
+        // when
+        val actions = deleteRequest("/city", existsIdDto)
+
+        // then
+        actions
+            .andExpectAll(
+                MockMvcResultMatchers.status().isOk,
+                MockMvcResultMatchers.jsonPath("name").value("notDuplicatedName")
+            )
+    }
+
+    private fun postRequest(requestUrl: String, content: Map<String, String>): ResultActions =
+        mvc.perform(
+            MockMvcRequestBuilders.post(requestUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(objectMapper.writeValueAsString(content))
+        )
+
+    private fun deleteRequest(requestUrl: String, content: Map<String, String>): ResultActions =
+        mvc.perform(
+            MockMvcRequestBuilders.delete(requestUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(objectMapper.writeValueAsString(content))
+        )
+}
